6d7868cc294f3175f9531c0402c080a1eb5d2fd4
Edgar de Paz <javierdpaz8@gmail.com>
2024-08-07 10:45:06 -0600

    Commit de Edgar

---
+++ Principal_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    // Edgar de Paz
+    int num;
+    printf("Ingrese un numero: >");
+    scanf("%d", &num);
+    if ((num % 2) == 0) {
+        printf("El numero es par");
+    } else {printf("El numero es impar");}
+    return 0;
+}
